f20fa1808017da73650ea7d3fe547fba5a140124
Max ChZ <chzanibal@hotmail.com>
2022-05-28 21:03:45 -0300

    agrear boton que agrega empleados  y redirecciona al home en la pagina proyectos

--- src/app/proyectos-component/proyectos-component.component.ts
+++ src/app/proyectos-component/proyectos-component.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../empleado.model';
+import { ServicioEmpleado2Service } from '../servicio-empleado2.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
 
 @Component({
   selector: 'app-proyectos-component',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router, private miServicio:ServicioEmpleadosService, private empleadosService:ServicioEmpleado2Service) { }
 
   ngOnInit(): void {
+    this.empleados=this.empleadosService.empleados;
+
+  }
+
+  volverHome(){
+this.router.navigate(['']);
+  }
+
+  empleados:Empleado[]=[];
+
+  cuadroNombre: string='';
+  cuadroApellido: string='';
+  cuadroCargo: string='';
+  cuadroSalario: number=0;
+
+  agregarEmpleado(){
+    let miEmpleado= new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    //Inyectamos el servicio antes que se agregue el emplado a la lista
+    // this.miServicio.mostrarMensaje('Nombre del empleado: '+ miEmpleado.nombre + ' Apellido del empleado: '+miEmpleado.apellido)
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
+    this.router.navigate(['']);
+
   }
 
 }
